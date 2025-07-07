8f15306245e08de7519a03526900be34a8e62286
RPelfresne <49885062+RPelfresne@users.noreply.github.com>
2025-07-07 18:40:01 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,8 +7,8 @@ radio.onReceivedString(function (receivedString) {
 input.onButtonPressed(Button.B, function () {
     _bouton = "B"
 })
-let msg_lu = ""
 let etat = 0
+let msg_lu = ""
 let msg = ""
 let _bouton = ""
 radio.setGroup(1)
@@ -21,16 +21,44 @@ basic.showLeds(`
     `)
 let etat_suivant = 0
 basic.forever(function () {
+    msg_lu = " "
     etat = etat_suivant
     msg_lu = msg
+    msg = " "
     if (etat == 1) {
         radio.sendString("B")
         basic.showString("B")
         basic.pause(100)
     } else if (etat == 2) {
-        basic.showString("RO")
+        basic.showString("R?")
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
     } else if (etat == 3) {
+        basic.showString("LR")
+        radio.sendString("LR")
+    } else if (etat == 4) {
         basic.showString("RO")
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            # # # # #
+            . # . # .
+            # . . . #
+            `)
+        basic.pause(200)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    } else if (etat == 5) {
         basic.showLeds(`
             . . # . .
             . # . . .
@@ -39,23 +67,16 @@ basic.forever(function () {
             . . # . .
             `)
         basic.pause(200)
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-    } else if (etat == 4) {
+    } else if (etat == 6) {
         radio.sendString("SR")
         basic.showLeds(`
             . . # . .
-            . # . . .
+            . . # . .
             # # # # #
-            . # . . .
+            . . # . .
             . . # . .
             `)
-    } else if (etat == 5) {
+    } else if (etat == 7) {
         radio.sendString("A")
         basic.showLeds(`
             . . . . .
@@ -82,18 +103,29 @@ basic.forever(function () {
             etat_suivant = 2
         }
     } else if (etat == 2) {
-        if (msg_lu == "FLR") {
+        if (_bouton == "A") {
+            _bouton = " "
             etat_suivant = 3
         }
     } else if (etat == 3) {
-        if (_bouton == "A") {
-            _bouton = " "
+        if (msg_lu == "DR") {
             etat_suivant = 4
         }
     } else if (etat == 4) {
+        if (msg_lu == "FLR") {
+            etat_suivant = 5
+        }
+    } else if (etat == 5) {
         if (_bouton == "A") {
             _bouton = " "
-            etat_suivant = 5
+            etat_suivant = 6
+        }
+    } else if (etat == 6) {
+        if (msg_lu == "ST") {
+            if (_bouton == "A") {
+                _bouton = " "
+                etat_suivant = 7
+            }
         }
     }
 })
